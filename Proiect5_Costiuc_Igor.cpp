266f26cc4e32bfadca6a3ecfdfe8c48e613c0867
Igor Costiuc <gast777@yahoo.com>
2022-03-22 11:15:14 +0200

    project 5

--- Proiect5_Costiuc_Igor.cpp
+++ Proiect5_Costiuc_Igor.cpp
@@ -1,61 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 #include <string.h>
-
-struct Sportsman {
-    int id;
-    char name[30];
-    int birthDay;
-    int birthMonth;
-    int birthYear;
-    char discipline[30];
-    int height;
-    int weight;
-};
-
-int main()
-{
-    char edit[2];
-    char editProperty[20];
-    struct Sportsman Sportsman;
-
-  printf("Enter Sportsman's ID, name, birthDay, birthMonth, birthYear, discipline, height, weight :\n");
-  scanf(
-        "%d %s %d %d %d %s %d %d", 
-        &Sportsman.id,
-        Sportsman.name,
-        &Sportsman.birthDay,
-        &Sportsman.birthMonth,
-        &Sportsman.birthYear,
-        Sportsman.discipline,
-        &Sportsman.height,
-        &Sportsman.weight
-    );
-
-    FILE *fptr;
-
-    if ((fptr = fopen("/Applications/code/programare-structurata/laborator-6/index.bin", "wb+")) == NULL){
-        printf("Error! opening file");
-
-        // Program exits if the file pointer returns NULL.
-        exit(1);
-    }
-
-    fwrite(&Sportsman, sizeof(struct Sportsman), 1, fptr); 
-
-    fread(&Sportsman, sizeof(struct Sportsman), 1, fptr); 
-    printf("ID: %d\nName: %s\nBirth: %d.%d.%d \nDiscipline: %s\nHeight: %d\nWeight: %d\n",
-        Sportsman.id,
-        Sportsman.name,
-        Sportsman.birthDay,
-        Sportsman.birthMonth,
-        Sportsman.birthYear,
-        Sportsman.discipline,
-        Sportsman.height,
-        Sportsman.weight
-    );
-
-    fclose(fptr);
-
-    return 0;
+#include <cstdlib>
+#include <conio.h>
+//6/16
+using namespace std;
+int NrCuvinte(char sir[]);
+int CuvintePalindroame(char sir[]);
+int CuvinteLungMin16(char sir[]);
+int NrCuvinte();
+int CuvintePalindroame();
+int CuvinteLungiMin16(char sir[]);
+int TransferCuvinteLungMin16(char sir[]);
+int EliminaCuvinteLungMin5(char sir[]);
+void ViewFile(char nume[]);
+void CopiaRezerva(char nume[]);
+int main ()
+{   
+    int opt;
+    char frac[81] = "Ana Virlan prieteneste cu Ana Cabc iar Colea Cazac - nu"
+    cout << "Proiect nr.5 elaborat de Costiuc Igor" << endl;
+    cout << "Tema: Siruri!" << endl;
+    do {
+        cout << "\n 1. Numarul de cuvinte dintr-o fraza";
+        cout << "\n 2. Cuvinte dintr-o fraza";
+        cout << "\n 3. Cuvintele de lungime cel putin 16 litere dintr-o fraza";
+        cout << "\n 4. Numarul cuvintelor dintr-un fisier";
+        cout << "\n 5. Cuvinte dintr-o fraza";
+        cout << "\n 6. Cuvintele de lungime cel putin 16 litere dintr-un fisier";
+        cout << "\n 7. De scris in alt fisier cuvintele >= 16 caractere";
+        cout << "\n 8. Copia de rezerva a fisierului";
+        cout << "\n 0. Stop!";
+        cout << "\n\n\t Alege de la 0 la 8";
+        cin >> opt;
+        switch(opt)
+        {
+            case 1;
+        }   
+        cout << "Click me"; getchar();
+    }  while(opt);
+        return 0;
+}
+int NrCuvinte(char sir[]) {
+    return 1;
 }
